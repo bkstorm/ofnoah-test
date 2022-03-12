f9a6414267fa0ead700788b535c31812518c1a9a
Nguyen Hoang <hoangnv@outlook.com>
2022-03-12 15:55:57 +0700

    feat: add @CurrentUser decorator

--- gateway/src/controllers/user.controller.ts
+++ gateway/src/controllers/user.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
+import { CurrentUser } from '../decorators/current-user.decorator';
 import { AuthGuard } from '../guards/auth.guard';
+import { User } from '../interfaces/user';
 
 @Controller('users')
 export class UserController {
   @UseGuards(AuthGuard)
   @Get('hello')
-  hello(@Req() req: Request): string {
-    return `Hello ${req['user'].email}`;
+  hello(@CurrentUser() user: User): string {
+    return `Hello ${user.email}`;
   }
 }
