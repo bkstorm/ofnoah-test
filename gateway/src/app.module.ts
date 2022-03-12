4a0750e485b69231d60a987babc8f4798c6deaef
Nguyen Hoang <hoangnv@outlook.com>
2022-03-12 15:40:41 +0700

    feat: add auth guard for gateway

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,13 +1,20 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 
 import { UserController } from './controllers/user.controller';
+import { AuthGuard } from './guards/auth.guard';
 import { AuthMiddleware } from './middlewares/auth.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [UserController],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
--- gateway/src/controllers/user.controller.ts
+++ gateway/src/controllers/user.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
+
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('users')
 export class UserController {
+  @UseGuards(AuthGuard)
   @Get('hello')
   hello(@Req() req: Request): string {
     return `Hello ${req['user'].email}`;
