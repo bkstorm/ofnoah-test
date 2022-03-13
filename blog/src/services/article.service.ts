8d79e5d2637006579b452c7427ebedcea537c29c
Nguyen Hoang <hoangnv@outlook.com>
2022-03-13 23:15:28 +0700

    feat(blog): save articles on Firestore

--- blog/src/services/article.service.ts
+++ blog/src/services/article.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Repository } from 'typeorm';
+import { Connection, DeleteResult, Repository } from 'typeorm';
+import { applicationDefault, App, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
 import { Article } from '../entities/article.entity';
 import { CreateArticleDTO } from '../interfaces/create-article.dto';
@@ -8,9 +10,16 @@ import { UpdateArticleDto } from '../interfaces/update-article.dto';
 
 @Injectable()
 export class ArticleService {
+  private defaultApp: App;
+
   constructor(
     @InjectRepository(Article) private articleRepository: Repository<Article>,
-  ) {}
+    private connection: Connection,
+  ) {
+    this.defaultApp = initializeApp({
+      credential: applicationDefault(),
+    });
+  }
 
   async findAll(): Promise<Article[]> {
     return this.articleRepository.find({ order: { id: 'DESC' } });
@@ -21,14 +30,33 @@ export class ArticleService {
   }
 
   async createArticle(data: CreateArticleDTO): Promise<Article> {
-    return this.articleRepository.save(data);
+    return this.connection.transaction(async (em) => {
+      const article = await em.getRepository(Article).save(data);
+      const doc = getFirestore()
+        .collection('article')
+        .doc(article.id.toString());
+      await doc.create(article);
+      return article;
+    });
   }
 
   async updateArticle(data: UpdateArticleDto): Promise<Article> {
-    return this.articleRepository.save(data);
+    return this.connection.transaction(async (em) => {
+      const article = await em.getRepository(Article).save(data);
+      const doc = getFirestore()
+        .collection('article')
+        .doc(article.id.toString());
+      await doc.update(article);
+      return article;
+    });
   }
 
   async deleteArticle(id: number): Promise<DeleteResult> {
-    return this.articleRepository.delete(id);
+    return this.connection.transaction(async (em) => {
+      const deleteResult = await em.getRepository(Article).delete(id);
+      const doc = getFirestore().collection('article').doc(id.toString());
+      await doc.delete();
+      return deleteResult;
+    });
   }
 }
