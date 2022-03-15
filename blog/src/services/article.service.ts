6b90c4ec16c4066a9c43c20be3b25b30d2ee8af4
Nguyen Hoang <hoangnv@outlook.com>
2022-03-15 14:22:34 +0700

    feat(blog): use env variables for firebase config

--- blog/src/services/article.service.ts
+++ blog/src/services/article.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Connection, DeleteResult, Repository } from 'typeorm';
-import { applicationDefault, App, initializeApp } from 'firebase-admin/app';
+import { cert, App, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
 import { Article } from '../entities/article.entity';
@@ -17,7 +17,11 @@ export class ArticleService {
     private connection: Connection,
   ) {
     this.defaultApp = initializeApp({
-      credential: applicationDefault(),
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      }),
     });
   }
 
