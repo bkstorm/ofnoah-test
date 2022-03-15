78fa66617dc3166ddf08d35780be30b622ec7d91
Nguyen Hoang <hoangnv@outlook.com>
2022-03-15 14:21:41 +0700

    feat(gateway): use env variables for firebase config

--- gateway/src/middlewares/auth.middleware.ts
+++ gateway/src/middlewares/auth.middleware.ts
@@ -4,7 +4,7 @@ import {
   Injectable,
   NestMiddleware,
 } from '@nestjs/common';
-import { applicationDefault, App, initializeApp } from 'firebase-admin/app';
+import { App, initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
 @Injectable()
@@ -13,7 +13,11 @@ export class AuthMiddleware implements NestMiddleware {
 
   constructor() {
     this.defaultApp = initializeApp({
-      credential: applicationDefault(),
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      }),
     });
   }
 
